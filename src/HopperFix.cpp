2e461ee14a423be299727011036cbfa943bd3b16
OEOTYAN <1124718975@qq.com>
2022-05-11 22:42:29 +0800

    update

--- src/HopperFix.cpp
+++ src/HopperFix.cpp
@@ -30,28 +30,31 @@ namespace {
 // fix hopper
 THook(gsl::span<gsl::not_null<Actor*>>*,
       MSSYM_MD5_21a49b5175e9c00856ce77825b121141,
-      void* bs,
+      void* blockSource,
       void* a2,
       uint64_t type,
       AABB* aABB) {
     auto volume =
         abs((aABB->max.x - aABB->min.x) * (aABB->max.y - aABB->min.y) *
             (aABB->max.z - aABB->min.z));
-    if (type != ITEM_ENTITY || volume < 0.9994f || volume > 1.000001f)
-        return original(bs, a2, type, aABB);
+    if (type != ITEM_ENTITY || volume < 0.9994f ||
+        volume > 1.000001f)  // isn't hopper or hopper_minecart
+        return original(blockSource, a2, type, aABB);
+
     if (abs(frac(aABB->min.y) - 0.625f) < 0.0001f &&
         abs(frac((aABB->max.x + aABB->min.x) / 2.0f) - 0.5f) < 0.0001f &&
         abs(frac((aABB->max.z + aABB->min.z) / 2.0f) - 0.5f) < 0.0001f) {
+        // is hopper
         aABB->max.y += 0.375f;
     }
     aABB->min.x = aABB->min.x - 0.125f;
     aABB->min.z = aABB->min.z - 0.125f;
     aABB->max.x = aABB->max.x + 0.125f;
     aABB->max.z = aABB->max.z + 0.125f;
-    auto list = original(bs, a2, type, aABB);
+    auto list = original(blockSource, a2, type, aABB);
     // std::cout << list->size();
     if (list->size() > 0 && list->size() != static_cast<std::size_t>(-1)) {
-        std::vector<Actor*> list2;
+        std::vector<Actor*> suckList;
         for (int i = 0; i < list->size(); i++) {
             auto item = list->operator[](i);
             auto pos = item->getPos();
@@ -60,38 +63,36 @@ THook(gsl::span<gsl::not_null<Actor*>>*,
             if (pos->x > aABB->min.x && pos->z > aABB->min.z &&
                 pos->x < aABB->max.x && pos->z < aABB->max.z) {
                 // std::cout << "true" << std::endl;
-                //  if (i > 0) {
-                //   std::swap(list->operator[](0), list->operator[](i));
-                //   std::swap(list->operator[](0), list->operator[](i));
-                list2.push_back(list->operator[](i));
-                //}
+                suckList.push_back(list->operator[](i));
             }
         }
 
-        if (list2.size() > 1) {
-            auto random = getRand(list2.size());
-            std::swap(list2[0], list2[random]);
+        if (suckList.size() > 1) {
+            auto random = getRand(suckList.size());
+            std::swap(suckList[0], suckList[random]);
         }
 
-        *((int64_t*)bs + 20) = *((int64_t*)bs + 19);  // clear
+        auto mEntityList = (int64_t*)((char*)blockSource + 152);
 
-        auto v6 = (int64_t*)((char*)bs + 152);
-        for (auto* i : list2) {
-            float** v39 = (float**)v6[1];
+        *((int64_t*)blockSource + 20) =
+            *((int64_t*)blockSource + 19);  // clear list
+
+        for (auto* i : suckList) {
+            float** v39 = (float**)mEntityList[1];
             int64_t v52;
             *(int64_t*)&v52 = (int64_t)i;
             *v39 = (float*)i;
-            v6[1] += 8i64;
+            mEntityList[1] += 8i64;
         }
-        auto v44 = (int64_t*)*v6;
-        auto v45 = (int64_t*)v6[1];
-        auto v46 = (int64_t*)*v6;
-        int64_t* v7;
-        v7 = (int64_t*)a2;
-        if ((int64_t*)*v6 == v45) {
+        auto v44 = (int64_t*)*mEntityList;
+        auto v45 = (int64_t*)mEntityList[1];
+        auto v46 = (int64_t*)*mEntityList;
+        int64_t* result;
+        result = (int64_t*)a2;
+        if ((int64_t*)*mEntityList == v45) {
         LABEL_69:
             auto v27 = v45 - v44;
-            *v7 = v27;
+            *result = v27;
         } else {
             while (*v46) {
                 if (++v46 == v45)
@@ -99,12 +100,8 @@ THook(gsl::span<gsl::not_null<Actor*>>*,
             }
         }
         // std::cout << "hooked" << std::endl;
-        return (gsl::span<gsl::not_null<Actor*>>*)v7;
+        return (gsl::span<gsl::not_null<Actor*>>*)result;
     }
 
     return list;
-    // if (list->size() < 2)
-    // return list;
-    // auto random = getRand(list->size());
-    // std::swap(list->operator[](0), list->operator[](random));
 }
